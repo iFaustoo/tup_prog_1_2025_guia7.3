dcf7283254618e1dadb23f6a307519bf57be4563
iFaustoo <faustoordoriguez10@gmail.com>
2025-06-16 16:48:26 -0300

    Add files via upload

---
+++ ej4_7_3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asdd7._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, nMayor = 0, nMenor = int.MaxValue;
+
+
+
+            for (int i = 1; i < 31; i++)
+            {
+                Console.WriteLine("Ingrese el número " + i);
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n > nMayor)
+                {
+                    nMayor = n;
+                }
+                else if (n < nMenor)
+                {
+                    nMenor = n;
+                }
+            }
+            Console.WriteLine($"El mayor número ingresado fue {nMayor}");
+            Console.WriteLine($"El menor número ingresado fue {nMenor}");
+
+        }
+    }
+}
